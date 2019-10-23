6b5ed8f62c53313d95f09bdadda85af256d4f19f
Johannes Bauer <joe@johannes-bauer.com>
2019-10-23 22:12:00 +0200

    Remove unused code
    Old, now unused code removed entirely.

--- server.c
+++ server.c
@@ -48,6 +48,13 @@
 #include "keydb.h"
 #include "signals.h"
 
+struct client_ctx_t {
+	struct generic_tls_ctx_t *gctx;
+	const struct keydb_t *keydb;
+	const struct host_entry_t *host;
+	int fd;
+};
+
 static int create_tcp_server_socket(int port) {
 	int s;
 	struct sockaddr_in addr;
@@ -79,184 +86,6 @@ static int create_tcp_server_socket(int port) {
 
 	return s;
 }
-
-#if 0
-static const struct keyentry_t *server_key;
-
-
-
-static bool unlock_disk(const struct diskentry_t *disk, const uint8_t *passphrase, int passphrase_length) {
-	char ascii_uuid[40];
-	sprintf_uuid(ascii_uuid, disk->disk_uuid);
-	log_msg(LLVL_INFO, "Trying to unlock disk %s with UUID %s", disk->devmapper_name, ascii_uuid);
-#ifdef DEBUG
-	fprintf(stderr, "Using %d bytes key for unlocking: ", passphrase_length);
-	dump_hex(stderr, passphrase, passphrase_length);
-	fprintf(stderr, "\n");
-#endif
-	if (is_luks_device_opened(disk->devmapper_name)) {
-		log_msg(LLVL_INFO, "Disk %s already unlocked, nothing to do.", disk->devmapper_name, ascii_uuid);
-		return true;
-	}
-	return open_luks_device_pw(disk->disk_uuid, disk->devmapper_name, passphrase, passphrase_length);
-}
-
-static bool all_disks_unlocked(const struct keyentry_t *keyentry) {
-	for (int i = 0; i < MAX_DISKS_PER_HOST; i++) {
-		if (keyentry->disk_keys[i].occupied && !is_luks_device_opened(keyentry->disk_keys[i].devmapper_name)) {
-			return false;
-		}
-	}
-	return true;
-}
-
-static bool tls_server(const struct keyentry_t *key, const struct options_t *options) {
-	if (all_disks_unlocked(key)) {
-		log_msg(LLVL_INFO, "Starting of server not necessary, all disks already unlocked.");
-		return true;
-	}
-
-	struct generic_tls_ctx_t gctx;
-	create_generic_tls_context(&gctx, true);
-
-	server_key = key;
-	{
-		char ascii_host_uuid[40];
-		sprintf_uuid(ascii_host_uuid, key->host_uuid);
-		SSL_CTX_use_psk_identity_hint(gctx.ctx, ascii_host_uuid);
-	}
-	SSL_CTX_set_psk_server_callback(gctx.ctx, psk_server_callback);
-
-	int tcp_sock = create_tcp_socket(options->port);
-	if (tcp_sock == -1) {
-		log_msg(LLVL_ERROR, "Cannot start server without server socket.");
-		free_generic_tls_context(&gctx);
-		return false;
-	}
-
-	int udp_sock = create_udp_socket();
-	if (tcp_sock == -1) {
-		log_msg(LLVL_ERROR, "Cannot broadcast without announcement UDP socket.");
-		close(tcp_sock);
-		free_generic_tls_context(&gctx);
-		return false;
-	}
-
-	log_msg(LLVL_DEBUG, "Created listening socket on port %d", options->port);
-	int tries = 0;
-	int failed_broadcast_cnt = 0;
-	while ((options->unlock_cnt == 0) || (tries < options->unlock_cnt)) {
-		struct sockaddr_in addr;
-		unsigned int len = sizeof(addr);
-
-		log_msg(LLVL_DEBUG, "Waiting for incoming connection...");
-		if (!announce_waiting_message(udp_sock, options->port, key)) {
-			failed_broadcast_cnt++;
-			if ((options->max_broadcast_errs != 0) && (failed_broadcast_cnt >= options->max_broadcast_errs)) {
-				log_msg(LLVL_ERROR, "Too many broadcast errors, aborting. Network unavailable?");
-				break;
-			}
-		}
-		if (!socket_wait_acceptable(tcp_sock, WAITING_MESSAGE_BROADCAST_INTERVAL_MILLISECONDS)) {
-			/* No connection pending, timeout. */
-			continue;
-		}
-
-		log_msg(LLVL_DEBUG, "Trying to accept connection...");
-		int client = accept(tcp_sock, (struct sockaddr*)&addr, &len);
-		if (client < 0) {
-			log_libc(LLVL_ERROR, "Unable to accept(2)");
-			close(udp_sock);
-			close(tcp_sock);
-			free_generic_tls_context(&gctx);
-			return false;
-		}
-
-		SSL *ssl = SSL_new(gctx.ctx);
-		SSL_set_fd(ssl, client);
-
-		if (SSL_accept(ssl) <= 0) {
-			ERR_print_errors_fp(stderr);
-		} else {
-			tries++;
-			log_msg(LLVL_DEBUG, "Client connected, waiting for data...");
-			while (true) {
-				struct msg_t msg;
-				int rxlen = SSL_read(ssl, &msg, sizeof(msg));
-				if (rxlen == 0) {
-					/* Client severed the connection */
-					break;
-				}
-				if (rxlen != sizeof(msg)) {
-					log_msg(LLVL_ERROR, "Truncated message (%d bytes) received, terminating connection. Expected %d bytes.", rxlen, sizeof(msg));
-					break;
-				}
-				msg_to_hbo(&msg);
-
-				if ((msg.passphrase_length == 0) || (msg.passphrase_length > MAX_PASSPHRASE_LENGTH)) {
-					log_msg(LLVL_FATAL, "Client sent malformed message indicating illegal passphrase length of %d bytes. Aborting connection.", msg.passphrase_length);
-					break;
-				}
-
-				/* Now check if this is one of they keys we're actually looking for */
-				bool found = false;
-				for (int i = 0; i < MAX_DISKS_PER_HOST; i++) {
-					if (!memcmp(key->disk_keys[i].disk_uuid, msg.disk_uuid, 16)) {
-						bool success = unlock_disk(&key->disk_keys[i], msg.passphrase, msg.passphrase_length);
-						log_msg(LLVL_DEBUG, "Unlocking of disk was %s", success ? "successful" : "unsuccessful");
-						found = true;
-						break;
-					}
-				}
-				if (!found) {
-					char ascii_uuid[40];
-					sprintf_uuid(ascii_uuid, msg.disk_uuid);
-					log_msg(LLVL_INFO, "Client sent passphrase for UUID %s; we were not expecting it. Ignored.", ascii_uuid);
-				}
-			}
-		}
-
-		SSL_free(ssl);
-		close(client);
-
-		/* Connection closed */
-		if (all_disks_unlocked(key)) {
-			log_msg(LLVL_INFO, "All disks successfully unlocked.");
-			break;
-		} else {
-			log_msg(LLVL_DEBUG, "At least one disk remains locked after communication.");
-		}
-	}
-
-	close(udp_sock);
-	close(tcp_sock);
-	free_generic_tls_context(&gctx);
-	return true;
-}
-#endif
-
-#if 0
-static unsigned int psk_server_callback(SSL *ssl, const char *identity, unsigned char *psk, unsigned int max_psk_len) {
-	if (max_psk_len < PSK_SIZE_BYTES) {
-		log_msg(LLVL_FATAL, "Server error: max_psk_len too small.");
-		return 0;
-	}
-	if (strcmp(identity, CLIENT_PSK_IDENTITY)) {
-		log_msg(LLVL_FATAL, "Server error: client identity '%s' unexpected (expected '%s').", identity, CLIENT_PSK_IDENTITY);
-		return 0;
-	}
-//	memcpy(psk, server_key->psk, PSK_SIZE_BYTES);
-	return PSK_SIZE_BYTES;
-}
-#endif
-
-struct client_ctx_t {
-	struct generic_tls_ctx_t *gctx;
-	const struct keydb_t *keydb;
-	const struct host_entry_t *host;
-	int fd;
-};
-
 
 static int psk_server_callback(SSL *ssl, const unsigned char *identity, size_t identity_len, SSL_SESSION **sessptr) {
 	struct client_ctx_t *ctx = (struct client_ctx_t*)SSL_get_app_data(ssl);
